bf09f9a4a6dcf8ea496a795e3de3ce1edda33f68
SanMapayi <mapayitaiwo@gmail.com>
2025-03-19 11:53:36 +0000

    removing boiler plate code and moving codes to the page and behaviour class

--- src/test/java/com/carchecking/pages/CarOutputComparisonPage.java
+++ src/test/java/com/carchecking/pages/CarOutputComparisonPage.java
@@ -1,10 +1,16 @@
 package com.carchecking.pages;
 
+import com.carchecking.base.TestBase;
 import com.carchecking.behaviour.GetMethods;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
-public class CarOutputComparisonPage {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class CarOutputComparisonPage extends TestBase {
 
     GetMethods getMethods = GetMethods.getInstance;
 
@@ -28,4 +34,33 @@ public class CarOutputComparisonPage {
     public String getYear() {
         return getMethods.getDetailValueByLabel("Year");
     }
+
+    public void enterRegNumberAndSearch(String regNumber, CarCheckingHomePage carCheckingHomePage) throws IOException {
+        carCheckingHomePage.enterRegNumber(regNumber);
+        carCheckingHomePage.clickCheckNowButton();
+        logger.info("Clicked 'Check Now' for: " + regNumber);
+    }
+
+    public void checkIfResultIsNotDisplayed(String regNumber, CarCheckingHomePage carCheckingHomePage) {
+        if (!carCheckingHomePage.isSearchResultDisplayed()) {
+            logger.error("Search result not displayed for: " + regNumber);
+            Assert.fail("Search result not displayed for: " + regNumber);
+        }
+    }
+
+    public void checkIfMapContainsKeys
+            (String regNumber, Map<String, List<String>> mapOfExpectedResultOutput) {
+        if (!mapOfExpectedResultOutput.containsKey(regNumber)) {
+            logger.warn("Registration number not found in expected results: " + regNumber);
+            Assert.fail("Registration number missing in expected output: " + regNumber);
+        }
+    }
+
+    public void enterRegAndSearchPage
+            (String regNumber, CarCheckingHomePage carCheckingHomePage, CarOutputComparisonPage carOutputComparisonPage) throws IOException {
+        carOutputComparisonPage.enterRegNumberAndSearch(regNumber, carCheckingHomePage);
+        carOutputComparisonPage.checkIfResultIsNotDisplayed(regNumber, carCheckingHomePage);
+        logger.info("Search result displayed for: " + regNumber);
+    }
+
 }
